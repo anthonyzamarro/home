73bad6ca2251c94a94da15c228852424cad308eb
Anthony <anthony.zamarro@gmail.com>
2025-08-19 15:29:55 -0400

    add: sample query from client to BE

---
+++ frontend/src/app/main.tsx
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+export const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
+
+
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { gql } from '@apollo/client'
+import { client  } from './main.tsx';
 
 export default function Home() {
+  client
+  .query({
+    query: gql`
+      query GetFoodItems {
+        foodItems {
+          id
+          name
+          quantity
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
   return (
     <div className={styles.page}>
       <main className={styles.main}>
