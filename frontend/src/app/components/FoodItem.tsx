596302d44392f65c5dc1df49c5077c8dd5e7a786
Anthony <anthony.zamarro@gmail.com>
2025-08-22 22:41:05 -0400

    add: start of first component

---
+++ frontend/src/app/components/FoodItem.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { React } from 'react'
+
+interface FootItem {
+	name: string
+	quantity: number
+}
+
+
+export const FoodItem: React.FC<FoodItem> = ({name, quantity}) => {
+	return (
+		<div>
+			<p>name: {name}</p>
+			<p>quantity: {quantity}</p>
+		</div>
+
+	)
+}
