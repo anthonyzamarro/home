80e15fdcf88d8007cf9993090e58098155f7c0f7
Anthony <anthony.zamarro@gmail.com>
2025-08-24 07:49:04 -0400

    fix: rendering of fetched data

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,12 @@ import { gql } from "@apollo/client"
 import { FoodItem } from "./components/FoodItem"
 import { useQuery } from "@apollo/client/react"
 
+interface FoodItem {
+  id: number
+  name: string
+  quantity: string
+}
+
 export default function Home() {
   const FOOD_ITEMS = gql`
     query GetFoodItems {
@@ -16,7 +22,7 @@ export default function Home() {
     }
   `
 
-  const { loading, error, data } = useQuery(FOOD_ITEMS)
+  const { loading, error, data: { foodItems } = { foodItems: [] } } = useQuery<{ foodItems: FoodItem[] }>(FOOD_ITEMS)
 
   if (loading) {
     return <p>Loading...</p>
@@ -26,13 +32,13 @@ export default function Home() {
     return <p>Error</p>
   }
 
-  console.log({ data })
-
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Home</h1>
-        <FoodItem name={""} quantity={""} />
+        {foodItems.map(({ id, name, quantity }: FoodItem) => {
+          return <FoodItem name={name} quantity={quantity} key={id} />
+        })}
       </main>
     </div>
   )
