c45904f5d9e95678e533e73b04457b1ab3bd0d74
Anthony <anthony.zamarro@gmail.com>
2025-09-18 08:43:34 -0400

    feat: update db config based on environment

--- backend/dist/db.js
+++ backend/dist/db.js
@@ -6,12 +6,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 import { Pool } from "pg";
+const sslConfig = process.env.ENV === "development" ? false : { rejectUnauthorized: false };
 const pool = new Pool({
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: false,
+    ssl: sslConfig,
 });
 export default pool;
